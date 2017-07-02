b884e7f4f2afe017daadda8e2fe1b4aae7a2875c
Emilia Wieczorek <wieczorekemily@gmail.com>
2017-07-01 19:10:13 -0700

    added problems28-31

--- intermediate_problems.sql
+++ intermediate_problems.sql
@@ -66,3 +66,52 @@ limit 3;
 # 28
 # High freight charges - last year
 
+set @last_year = (select date_add(max(p.OrderDate), interval -1 year) from orders p);
+
+select p.ShipCountry, cast(avg(p.Freight) as decimal(10,4)) as AverageFreight 
+from Orders p 
+where p.OrderDate >= @last_year
+group by p.ShipCountry 
+order by avg(p.Freight) desc 
+limit 3;
+
+# 29
+# Inventory List
+
+select e.EmployeeID, e.LastName, o.OrderID, p.ProductName, d.Quantity 
+from orders o 
+inner join OrderDetails d using (OrderID) 
+inner join Products p using (ProductID) 
+inner join Employees e on o.EmployeeID = e.EmployeeID
+order by o.OrderID, p.ProductID;
+
+# 30
+# Customers with no orders
+
+select c.CustomerID as Customers_CustomerID, o.CustomerID as Orders_CustomerID 
+from Customers c 
+left join Orders o 
+using (CustomerID) 
+where o.CustomerID is null;
+
+# 31
+# Customers with no orders for EmployeeID 4
+
+select c.CustomerID, o.CustomerID 
+from Customers c 
+left join Orders o
+on c.CustomerID = o.CustomerID and o.EmployeeID = 4
+where o.CustomerID is null;
+
+# alternate solution
+select c.CustomerID, b.CustomerID  
+from Customers c  
+left join  
+(select o.CustomerID from orders o where o.EmployeeID = 4) b 
+on c.CustomerID = b.CustomerID 
+where b.CustomerID is null;
+
+# another alternate solution
+select c.CustomerID 
+from Customers c 
+where c.CustomerID not in (select o.CustomerID from Orders o where o.EmployeeID = 4);
